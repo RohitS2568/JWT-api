22c8ae9fb92e579a1c71e0f39802a26e8fe7c699
Rohit Singh Shekhawat <92093950+RohitS2568@users.noreply.github.com>
2021-10-21 12:51:26 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,27 +3,27 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     
     Username:{
-        type: String,
-        required : true,
+        type: String
+        
     },
-    
+      
     email:{
         type: String,
-        required : true,
+        
         unique:true,
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
     password:{
         type: String,
-        required : true,
+        
     },
     confirm_password:{
-        type: String,
-        required : true,
+        type: String
+        
     },
     token:{
         type : String
-    },
+    }
 });
 
 module.exports = mongoose.model('user', userSchema);
