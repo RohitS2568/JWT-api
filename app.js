713cb742da6eaf149bd673a5226cfa2e80084a04
Rohit Singh Shekhawat <92093950+RohitS2568@users.noreply.github.com>
2021-10-21 11:24:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const userRoute = require('../route/user');
+const userRoute = require('./route/user');
 
 
-mongoose.connect('mongodb://localhost:27017/database', {useNewUrlParser: true}, (err) => {
+mongoose.connect('mongodb+srv://Admin123:Admin123@cluster0.wqrjm.mongodb.net/JWT?retryWrites=true&w=majority', {useNewUrlParser: true}, (err) => {
 if (!err) {console.log('MongoDB Connection Succeeded.') }
-else {console.log('Error in DB connection:' + err) }
+else {console.log('Error in DB connection:' + err) }           
 });
-
+                                                       
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.use('/user',userRoute)
-
 
 module.exports = app;
